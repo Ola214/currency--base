bef1d27338addc668d3212141e0c83663f6677fc
Ola214 <aleksandra.krysinska.94@gmail.com>
2025-11-04 22:13:09 +0100

    First tests

---
+++ src/utils/convertPLNToUSD.js
@@ -0,0 +1,14 @@
+export const convertPLNToUSD = (PLN) => {
+  if (PLN === null || PLN === undefined) return NaN;
+  if (typeof PLN === 'string') return NaN;
+  if (typeof PLN !== 'number') return 'Error';
+  if (PLN < 0) PLN = 0;
+
+  const PLNtoUSD = PLN / 3.5;
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+}
