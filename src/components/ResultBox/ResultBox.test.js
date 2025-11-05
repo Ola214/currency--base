953d3c977bfc8c76553b722e62f4598b504d9354
Ola214 <aleksandra.krysinska.94@gmail.com>
2025-11-05 17:23:48 +0100

    Testing ResultBox 3

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import ResultBox from './ResultBox';
 
@@ -13,3 +13,21 @@ import ResultBox from './ResultBox';
         render(<ResultBox from="PLN" to="USD" amount={100} />);
     });
   });
+
+  describe('ResultBox', () => {
+  const testCases = [
+    { amount: 100, from: 'PLN', to: 'USD', expectedText: 'PLN 100.00 = $28.57' },
+    { amount: 200, from: 'PLN', to: 'USD', expectedText: 'PLN 200.00 = $57.14' },
+    { amount: 345, from: 'PLN', to: 'USD', expectedText: 'PLN 345.00 = $98.57' },
+    { amount: 50, from: 'PLN', to: 'USD', expectedText: 'PLN 50.00 = $14.29' },
+  ];
+
+  for (const testObj of testCases) {
+    it(`should render proper info about conversion when ${testObj.from} -> ${testObj.to} and amount=${testObj.amount}`, () => {
+      render(<ResultBox from={testObj.from} to={testObj.to} amount={testObj.amount} />);
+      const output = screen.getByTestId('output');
+      expect(output).toHaveTextContent(testObj.expectedText);
+      cleanup();
+    });
+  }
+});
